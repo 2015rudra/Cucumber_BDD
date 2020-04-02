ec7ba35b7dd75d8a65a4d144c2b50878f84ed8a8
Rupesh <rupesh3101.kadam@gmail.com>
2020-04-02 21:11:18 +0530

    Added Tagging

--- src/main/java/Runner/TestRunner.java
+++ src/main/java/Runner/TestRunner.java
@@ -7,12 +7,13 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = {"src/main/java/Features"}
+		features = {"src/main/java/Features/tagging.feature"}
 		,glue = {"stepDefinition"},
 		format = {"pretty","html:test-output", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"},
-		dryRun = false,
+		dryRun = true,
 		monochrome = true,
-		strict = true		
+		strict = true,
+		tags = {"@End2End"}
 		)
 
 
